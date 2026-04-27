091139548aa72f5c1de0fbd7779a0a025a8d0369
Aiman <aimanfatima384@gmail.com>
2026-04-27 11:02:45 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,35 @@
 // Major Project
-// Your Name
-// Date
-//
+// Aiman Fatima Adnan
+// 27/04/2026
+
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+let stockData;
+let request = require('request');
+let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=GKYRF5RF3G9HUU0U';
+
+request.get({
+  url: url,
+  json: true,
+  headers: {'User-Agent';'request'}
+}, (err, res, data) = > {
+  if (err) {
+    console.log('Error:', err);
+  } 
+  else if (res.statusCode !== 200) {
+    console.log('Status;', res.statusCode);
+  } 
+  else {
+    console.log(data);
+  }
+}
+})
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 50);
+  background(0, 0, 51);
 }
